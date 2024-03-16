a4228ad04ddca7438a9c2d528fb98ccfb72cc455
thanhpv1999 <thanhpv174223@gmail.com>
2024-03-16 19:27:44 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -10,30 +10,32 @@
 #define TAG "main"
 using namespace std;
 
+struct Example {
+    uint32_t d;
+    uint16_t b;
+    uint16_t c;
+    uint8_t a;
+};
+
+uint8_t arr[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
+
 int main(){
     cout << "Hello winDev:" << endl;
 
-    LinkedList list;
+    Example ex;
+    cout << "Kich thuoc cua struct Example: " << sizeof(ex) << " byte" << endl;
+    // // In ra địa chỉ của các thành viên của struct
+    // cout << "Dia chi cua bien a: " << (void*)&ex.a << endl;
+    // cout << "Dia chi cua bien b: " << (void*)&ex.c << endl;
+    // cout << "Dia chi cua bien c: " << (void*)&ex.b << endl;
 
-    list.append(1);
-    list.prepend(0);
-    list.append(2);
-    list.insertAfter(list.search(1), 1);
-    list.insertAfter(list.search(2), 1);
+    Example *p_set = (Example *)(&arr[0]);
+    cout << "Dia chi cua bien a: " << hex << setw(2) << p_set->a << endl;
+    cout << "Dia chi cua bien b: " << hex << setw(4) << p_set->b << endl;
+    cout << "Dia chi cua bien c: " << hex << setw(4) << p_set->c << endl;
+    cout << "Dia chi cua bien c: " << hex << setw(8) << p_set->d << endl;
 
-    std::cout << "Danh sach sau khi them: ";
-    list.printList();
-
-    list.remove(1);  // Xóa một node với giá trị 1
-
-    std::cout << "Danh sach sau khi xoa: ";
-    list.printList();
-
-    // Clear the list
-    list.clear();
-    std::cout << "Danh sach sau khi xoa toan bo: ";
-    list.printList();
-
+    printf("%x-%x-%x-%x\n", p_set->a, p_set->b, p_set->c, p_set->d);
 
     return 0;
 }
