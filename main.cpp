469f215560297f49d2dfb42db20a70f212741313
thanhpv1999 <thanhpv174223@gmail.com>
2024-03-18 08:30:27 +0700

    thanhtest

--- main.cpp
+++ main.cpp
@@ -5,33 +5,23 @@
 #include "./library2/req/req.h"
 #include "./library/squa/squa.h"
 #include "./library/linkedList/linkedList.h"
+#include <cstring>
 
 #define VERSION "1.0.2"
 #define TAG "main"
 using namespace std;
 
 struct Example {
-    char a;
-    int b;
-    char c;
+    uint8_t a;
+    uint8_t b;
+    uint32_t c;
 };
 
 uint8_t arr[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
 
-int main(){
-    cout << "Hello winDev:" << endl;
-
-    Example ex;
-    // cout << "Kich thuoc cua struct Example: " << sizeof(ex) << " byte" << endl;
-    // // In ra địa chỉ của các thành viên của struct
-    // cout << "Dia chi cua bien a: " << (void*)&ex.a << endl;
-    // cout << "Dia chi cua bien b: " << (void*)&ex.c << endl;
-    // cout << "Dia chi cua bien c: " << (void*)&ex.b << endl;
-
-    Example *temp = (Example *)&arr;
-    cout << "Dia chi cua bien a: " << temp->a << endl;
-    cout << "Dia chi cua bien b: " << temp->b << endl;
-    cout << "Dia chi cua bien c: " << temp->c << endl;
-
+int main() {
+    char str[] = "abcdefgh";
+    memmove(str + 4, str + 2, 4); // Di chuyển "cdef" tới vị trí sau "ab"
+    std::cout << "Result: " << str << std::endl;
     return 0;
 }
