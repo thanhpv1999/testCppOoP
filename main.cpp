1eaf4220de84efc01e33429cb0e449d474df6d9a
thanhpv1999 <thanhpv174223@gmail.com>
2024-03-24 22:36:22 +0700

    enum

--- main.cpp
+++ main.cpp
@@ -9,12 +9,27 @@
 #include "./library/linkedList/linkedList.h"
 #include "./library/checkPoint/checkPoint.h"
 
+enum mau {
+    xanh,
+    thanh,
+    vang
+};
+
 int main() {
 
     checkPoint *xetDiem = new checkPoint(1,2,3,4,5,6);
     xetDiem->checkLine();
     xetDiem->midpointAndBetween();
     cout << "dien tich: " << xetDiem->areaTri() << endl;
+
+    cout << "t1: " << sizeof(mau) << endl;
+    cout << "t2: " << xanh <<" "<<thanh << endl;
+
+    mau t, m;
+    t= xanh;
+    m= mau(10);
+    cout << "t3: " << t << endl;
+    cout << "t4: " << m << endl;
 
     return 0;
 }
